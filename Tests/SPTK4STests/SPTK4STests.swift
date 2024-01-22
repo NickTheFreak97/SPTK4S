588e737c4b8f4d19e4a3a72547d6628e60626cac
NickTheFreak97 <niccolo.dellarocca@gmail.com>
2024-01-22 13:48:41 +0100

    Initial Commit

---
+++ Tests/SPTK4STests/SPTK4STests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SPTK4S
+
+final class SPTK4STests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
